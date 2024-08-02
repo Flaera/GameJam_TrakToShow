2be5c0e48c3163c936a4d2dc61cd836fc236d7d8
Flaera <rafael2mcont@gmail.com>
2024-08-02 10:52:31 -0300

    Changes in clamp of selector.

--- godot/scenes/main_menu/main_menu.gd
+++ godot/scenes/main_menu/main_menu.gd
@@ -4,11 +4,11 @@ extends Control
 
 
 func _on_BTN_NEW_GAME_pressed():
-	get_tree().change_scene("res://scenes/selector_difficult/select_dificult.tscn")
+	get_tree().change_scene_to_file("res://scenes/selector_difficult/select_dificult.tscn")
 
 
 func _on_BTN_CONTINUE_pressed():
-	get_tree().change_scene("res://scenes/camps/camp0.tscn")
+	get_tree().change_scene_to_file("res://scenes/camps/camp0.tscn")
 
 
 func _on_BTN_QUIT_pressed():
